69f1dcd22b092e47f5b4628dcff19830d07bfdde
MuhammadGhulamMuhiuddin <muhammadghulammuhiuddin791@gmail.com>
2025-02-04 14:10:48 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.47.12/+esm";
 
 
-const projectUrl = "https://dgvgxfszkiseozxzmvyv.supabase.co";
-const projectApiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRndmd4ZnN6a2lzZW96eHptdnl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY0ODg1NDgsImV4cCI6MjA1MjA2NDU0OH0.qwP-8E-Z52fNR63RNl3eTnTownSy_q7Keilcc3g_INY";
+const projectUrl = "Your Project URl";
+const projectApiKey = "Your ProjectAPI Key";
 
 const supabase = createClient(projectUrl, projectApiKey);
 
